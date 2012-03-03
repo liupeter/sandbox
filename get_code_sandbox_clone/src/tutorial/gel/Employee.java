fe533cd86060548e3fe11429245dc50e93a145de
Peter Liu <peter.liu@senecacollege.ca>
2012-03-03 11:21:49 -0500

    README changed Employee class changed

---
+++ get_code_sandbox_clone/src/tutorial/gel/Employee.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package tutorial.gel;
+
+/**
+ * @author peter liu
+ *
+ */
+public class Employee {
+
+	/**
+	 * @param args
+	 */
+	private String name;
+	
+	public Employee() { name = "Dr. Peter Liu"; }
+	
+	public String toString() { return "employee name" + name; }
+	
+	public String getName() { return name; }
+	
+	public void setName( String name) { this.name = name; }
+	
+	// the main method
+	public static void main(String[] args) {
+		Employee emp = new Employee();
+		System.out.println( emp );
+		
+		emp.setName( "Dr. Pepper" );
+		System.out.println( emp.getName());
+
+	}
+
+}
